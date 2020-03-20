f8beb751341be60e96e67ee802301b8c043577d3
taka8rie <614877479@qq.com>
2020-03-20 14:29:50 +0800

    进行租房操作

---
+++ youtube/src/components/visit/ShowForm.vue
@@ -0,0 +1,97 @@
+<template>
+  <div>
+    <i class="el-icon-circle-plus-outline"  @click="dialogFormVisible = true"></i>
+    <el-dialog
+      title="房屋详情"
+      :visible.sync="dialogFormVisible"
+      @close="clear">
+      <el-form v-model="form" style="text-align: left" ref="dataForm">
+        <el-form-item label="房屋面积" :label-width="formLabelWidth" prop="houseArea">
+          <el-input v-model="form.houseArea" autocomplete="off" placeholder="这里填房屋面积"></el-input>
+        </el-form-item>
+        <el-form-item label="房屋状态" :label-width="formLabelWidth" prop="houseStatus">
+          <el-input v-model="form.houseStatus" autocomplete="off" placeholder="0:“待租” 1:“已出租”
+"></el-input>
+        </el-form-item>
+        <el-form-item label="房屋价格" :label-width="formLabelWidth" prop="soldPrice">
+          <el-input v-model="form.soldPrice" autocomplete="off"></el-input>
+        </el-form-item>
+<!--        <el-form-item label="提交日期" :label-width="formLabelWidth" prop="lastupdateTime">-->
+<!--          <el-input v-model="form.lastupdateTime" autocomplete="off"></el-input>-->
+<!--        </el-form-item>-->
+
+
+        <el-form-item label="房屋地点" :label-width="formLabelWidth" prop="houseAddr">
+          <el-input v-model="form.houseAddr" autocomplete="off"></el-input>
+        </el-form-item>
+<!--        <el-form-item label="房屋图片" :label-width="formLabelWidth" prop="houseCover">-->
+<!--          <el-input v-model="form.houseCover" autocomplete="off" placeholder="图片 URL"></el-input>-->
+<!--          <img-upload @onUpload="uploadImg" ref="imgUpload"></img-upload>-->
+<!--        </el-form-item>-->
+        <el-form-item label="房主要求" :label-width="formLabelWidth" prop="addNote">
+          <el-input type="textarea" v-model="form.addNote" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="房屋类型" :label-width="formLabelWidth" prop="houseType">
+          <el-select v-model="form.houseType" placeholder="请选择分类">
+            <el-option label="别墅" value="1"></el-option>
+            <el-option label="普通" value="2"></el-option>
+            <el-option label="3DK" value="3"></el-option>
+            <el-option label="其他" value="4"></el-option>
+         </el-select>
+        </el-form-item>
+        <el-form-item prop="houseNumber" style="height: 0">
+          <el-input type="hidden" v-model="form.houseNumber" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="gotolink">订购</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "ShowForm",
+      data () {
+        return {
+          dialogFormVisible: false,
+          form: {
+            houseNumber: '',
+            houseAddr: '',
+            houseType: '',
+            houseArea: '',
+            houseStatus: '',
+            houseCover: '',
+            lastupdateTime: '',
+            soldPrice:'',
+            addNote:'',
+          },
+          formLabelWidth: '120px'
+        }
+      },
+      methods: {
+        clear () {
+          this.form = {
+            houseNumber: '',
+            houseAddr: '',
+            houseType: '',
+            houseArea: '',
+            houseStatus: '',
+            houseCover: '',
+            lastupdateTime: '',
+            soldPrice:'',
+            addNote:'',
+          }
+        },
+        gotolink () {
+        this.$router.replace('/order')
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ youtube/src/components/visit/visitHouse.vue
@@ -0,0 +1,151 @@
+<template>
+    <div>
+      <el-row style="height: 840px;">
+        <search-bar @onSearch="searchResult" ref="searchBar"></search-bar>
+        <el-tooltip effect="dark" placement="right"
+                    v-for="item in houses.slice((currentPage-1)*pageSize,currentPage*pageSize)" :key="item.houseNumber">
+          <!-- house没有id,只有houseNumber       v-for="item in houses" :key="item.id">-->
+          <p slot="content" style="font-size: 14px;margin-bottom: 6px;">{{item.addNote}}</p>
+          <p slot="content" style="font-size: 13px;margin-bottom: 6px">
+            <span>房屋状态{{item.houseStatus}}</span>
+            <span>房屋类型{{item.houseType}}</span>
+            <span>房屋面积{{item.houseArea}}</span>
+          </p>
+          <p slot="content" style="width: 300px" class="abstract">{{item.addNote}}</p>
+          <el-card style="width: 135px;margin-bottom: 20px;height: 233px;float: left;margin-right: 15px" class="house"
+                   bodyStyle="padding:10px" shadow="hover">
+            <div class="cover" @click="editHouse(item)">
+              <img :src="item.houseCover" alt="封面">
+            </div>
+            <div class="info">
+              <div class="houseAddr">
+                <a href="">房屋地址:{{item.houseAddr}}</a>
+              </div>
+            </div>
+            <div class="lastupdate">{{item.lastupdateTime}}</div>
+          </el-card>
+        </el-tooltip>
+        <show-form @onSumbit="loadHouses()" ref="edit"></show-form>
+      </el-row>
+      <el-row>
+        <el-pagination
+          @current-change="handleCurrentChange"
+          :current-page="currentPage"
+          :page-size="pageSize"
+          :total="houses.length">
+        </el-pagination>
+      </el-row>
+    </div>
+ </template>
+
+
+<script>
+  import SearchBar from "../house/SearchBar";
+  import ShowForm from "./ShowForm";
+    export default {
+        name: "visitHouse",
+      components:{SearchBar,ShowForm},
+      data(){
+        return {
+          houses:[],
+          currentPage:1,
+          pageSize:17
+        }
+      },
+      mounted:function () {
+        this.loadHouses()
+      },
+      methods:{
+        loadHouses(){
+          var _this = this
+          this.$axios.get('/houses').then(resp => {
+            if (resp && resp.status === 200) {
+              _this.houses = resp.data
+            }
+          })
+        },
+        handleCurrentChange: function (currentPage) {
+          this.currentPage = currentPage
+          console.log(this.currentPage)
+        },
+        searchResult () {
+          var _this = this
+          this.$axios
+            .get('/search?keywords=' + this.$refs.searchBar.keywords, {
+            }).then(resp => {
+            if (resp && resp.status === 200) {
+              _this.houses = resp.data
+            }
+          })
+        },
+        editHouse(item) {
+          this.$refs.edit.dialogFormVisible = true
+          this.$refs.edit.form = {
+            houseNumber: item.houseNumber,
+            houseAddr: item.houseAddr,
+            houseType: item.houseType,
+            houseArea: item.houseArea,
+            houseStatus: item.houseStatus,
+            houseCover: item.houseCover,
+            lastupdateTime: item.lastupdateTime,
+            soldPrice:item.soldPrice,
+            addNote:item.addNote,
+          }
+        }
+      }
+    }
+</script>
+
+<style scoped>
+  .cover{
+    width: 115px;
+    height: 172px;
+    margin-bottom: 7px;
+    overflow: hidden;
+    cursor: pointer;
+  }
+
+  img {
+    width: 115px;
+    height: 172px;
+    /*margin: 0 auto;*/
+  }
+
+  .houseAddr {
+    font-size: 14px;
+    text-align: left;
+  }
+
+  .lastupdate {
+    color: #333;
+    width: 102px;
+    font-size: 13px;
+    margin-bottom: 6px;
+    text-align: left;
+  }
+
+  .abstract {
+    display: block;
+    line-height: 17px;
+  }
+
+  .el-icon-delete {
+    cursor: pointer;
+    float: right;
+  }
+
+  .switch {
+    display: flex;
+    position: absolute;
+    left: 780px;
+    top: 25px;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  a:link, a:visited, a:focus {
+    color: #3377aa;
+  }
+</style>
