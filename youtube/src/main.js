e968c0a3b542d42c921e1b6941fbd357a7813ce8
taka8rie <614877479@qq.com>
2020-04-10 16:55:00 +0800

    实现必须登录才能进入页面

--- youtube/src/main.js
+++ youtube/src/main.js
@@ -5,19 +5,129 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import toastMessage from "./components/alert/ToastMessage";
+import visitHouse from "./components/visit/visitHouse";
+import  Vuex from 'vuex'
+import store from './store'
+
+// import store from './store'
+Vue.use(toastMessage)
+// Vue.use(Vuex)
 
 var axios=require('axios')
 axios.defaults.baseURL='http://localhost:8443/api'
 Vue.prototype.$axios=axios
 Vue.config.productionTip = false
 
+//尝试获取session的值
+Vue.config.debug=true;
+axios.defaults.withCredentials=true//前端带上cookies
+
+
 Vue.use(ElementUI)
+
+//使用钩子函数判断是否拦截,在访问每一个路由前调用
+router.beforeEach((to,from,next)=>{
+  // if (store.state.user.username && to.path.startsWith('/login')) {
+  //   initAdminMenu(router,store)
+  // }
+  // 已登录状态下访问 login 页面直接跳转到后台首页  ---->这里需要修改/login和dashboard, 尝试修改startwith
+  //从'/login'修改为‘/’表示已经登录
+  // if (store.state.user.username && to.path.startsWith('/center')) {
+  //   next({
+  //     path: '/admin'//尝试在这里修改了路径
+  //   })
+  // }
+  // if (to.meta.requireAuth) {
+  //   if (store.state.user.username) {
+  //     axios.get('/authentication').then(resp=>{
+  //       if(resp) next()
+  //     })
+  //   }else {
+  //     next({
+  //       //这里尝试改为了空,自己设置的登录页面就是‘/’
+  //       path:'login',
+  //       query:{redirect:to.fullPath}
+  //     })
+  //   }
+  // }else {
+  //   next()
+  // }
+
+
+
+
+  //首先判断访问的路径是否需要被拦截进行登录
+  if (to.meta.requireAuth) {
+    //判断 store 里有没有存储 user 的信息，如果存在，则放行
+    if (store.state.user) {
+      axios.get('/authentication').then(resp=>{
+          console.log(resp)
+        if (resp.data) {
+          next();
+        }
+      })
+    }else{//否则跳转到登录页面
+      //并存储访问的页面路径（以便在登录后跳转到访问页）
+      next({
+        path:'/login',//应该要修改这个路径
+        query:{redirect:to.fullPath}
+      })
+    }
+  }else {
+    next();
+  }
+})
+
+const initAdminMenu = (router, store) => {
+  // 防止重复触发加载菜单操作
+  if (store.state.adminMenus.length > 0) {
+    return
+  }
+  axios.get('/menu').then(resp => {
+    if (resp && resp.status === 200) {
+      var fmtRoutes = formatRoutes(resp.data.result)
+      router.addRoutes(fmtRoutes)
+      store.commit('initAdminMenu', fmtRoutes)
+    }
+  })
+}
+
+
+
+const formatRoutes = (routes) => {
+  let fmtRoutes = []
+  routes.forEach(route => {
+    if (route.children) {
+      route.children = formatRoutes(route.children)
+    }
+
+    let fmtRoute = {
+      path: route.path,
+      component: resolve => {
+        require(['./components/admin/' + route.component + '.vue'], resolve)
+      },
+      name: route.name,
+      nameZh: route.nameZh,
+      iconCls: route.iconCls,
+      meta: {
+        requireAuth: true
+      },
+      children: route.children
+    }
+    fmtRoutes.push(fmtRoute)
+  })
+  return fmtRoutes
+}
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   render:h=>h(App),
   router,
-  components: { App },
+  store,
+  components: { App},
   template: '<App/>'
 })
+
+
--- youtube/src/router/index.js
+++ youtube/src/router/index.js
@@ -1,18 +1,146 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from "../components/Login";
-import Appindex  from "../components/home/Appindex";
+import Register from "../components/Register";
+import Home from "../components/home/Home";
+import HouseIndex from "../components/house/HouseIndex";
+import uploadHouse from "../components/zufang/uploadHouse";
+import visitHouse from "../components/visit/visitHouse";
+import Order from "../components/visit/Order";
+import ShowForm from "../components/visit/ShowForm";
+import ShowHouse from "../components/zufang/ShowHouse";
+import AdminIndex from "../components/admin/AdminIndex";
+import AdminMenu from "../components/admin/AdminMenu";
+import Header from "../components/admin/Header";
 
 Vue.use(Router)
 
 export default new Router({
+  //改为history模式
+  mode: 'history',
   routes: [
     {
-      path: '/index',
-      component: Appindex
-    },{
       path: '/login',
       component:Login
+    },{
+      path: '/register',
+      component:Register
+    },//以下为尝试新增
+    {
+    path:'/home',
+      component:Home,
+      redirect:'/index',
+      children:[{
+        path: '/house',
+        component:HouseIndex,
+        meta:{requireAuth: true}
+      },{
+      path:'/zufang',
+        component:uploadHouse,
+        meta:{requireAuth:true}
+      },{
+        path:'/index',
+        component:visitHouse,
+        meta:{requireAuth: true}
+      },{
+        path: '/order',
+        name:'order',
+        component:Order,
+        meta:{requireAuth: true},
+      },{
+        path: '/showform',
+        component:ShowForm,
+        meta:{requireAuth: true}
+      },
+      ]
+    },
+    // 4/10新增
+    {
+    path:'/admin',
+      component:AdminIndex,
+      meta:{
+      requireAuth:true
+      }
     }
   ]
 })
+
+// 用于创建默认路由
+export const createRouter = routes => new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Default',
+      redirect: '/home',
+      component: Home
+    },
+    {
+      // home页面并不需要被访问，只是作为其它组件的父组件
+      path: '/home',
+      name: 'Home',
+      component: Home,
+      redirect: '/index',
+      children: [
+        {
+          path: '/house',
+          component:HouseIndex,
+          meta:{requireAuth: true}
+        },{
+          path:'/zufang',
+          component:uploadHouse,
+          meta:{requireAuth:true}
+        },{
+          path:'/index',
+          component:visitHouse,
+          meta:{requireAuth: true}
+        },{
+          path: '/order',
+          name:'order',
+          component:Order,
+          meta:{requireAuth: true},
+        },{
+          path: '/showform',
+          component:ShowForm,
+          meta:{requireAuth: true}
+        },//已修改为自身
+      ]
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    },
+    {
+      path: '/admin',
+      name: 'Admin',
+      component: AdminIndex,
+      meta: {
+        requireAuth: true
+      },
+      // children: [
+        // {
+        //   path: '/admin/dashboard',
+        //   name: 'Dashboard',
+        //   component: DashBoard,
+        //   meta: {
+        //     requireAuth: true
+        //   }
+        // }
+      // ]
+    },
+    {
+      path: '*',
+      component: Error404
+    }
+  ]
+})
+
+
+
+
